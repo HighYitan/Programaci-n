e61c4fa944c688f6e9fb075a9b2a1f2ff07c1acf
Khristian <cfl01@iesemilidarder.com>
2024-04-24 16:37:08 +0200

    3versiones

--- UD12/ACT12/src/ACT12_2/Main.java
+++ UD12/ACT12/src/ACT12_2/Main.java
@@ -1,5 +1,4 @@
 package ACT12_2;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,7 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 /**
  *
  * @author alumnat
---
+++ UD12/ACT12/src/ACT12_2/Main2.java
@@ -0,0 +1,19 @@
+package ACT12_2;
+
+import java.io.File;
+
+/**
+ *
+ * @author alumnat
+ */
+public class Main2 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String win = "", lin = "/home/alumnat/Escriptori/temp/ACT12_2.html";
+        File nomArxiu = new File(lin);
+        int ini = 0, fin = 0;
+        
+    }   
+}
